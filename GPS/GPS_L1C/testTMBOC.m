fd08ece3bf7787496da7248031056720ddd7d777
alexander-lam <81650765+alexander-lam@users.noreply.github.com>
2023-11-30 22:58:05 -0500

    Update with filtering and higher sampling freqs

--- GPS/GPS_L1C/testTMBOC.m
+++ GPS/GPS_L1C/testTMBOC.m
@@ -1,70 +1,91 @@
 codeFreq = 1.023e6;
 codeLength = 10230;
+c = 299792458; % m/s
 PRN = 1;
 
 % Get a vector with the pilot BOC(1,1) spreading waveform replicated 10x
 pilotBOC11 = repmat(generateL1Cpboc11code(PRN), 1, 10);
 pilotBOC61 = repmat(generateL1Cpboc61code(PRN), 1, 10);
 
-% Generate replica codes at 20 Msps and 2Msps
+% Generate replica codes at 50 Msps
 replicaBOC11 = generateL1Cpboc11code(PRN);
 replicaBOC61 = generateL1Cpboc61code(PRN);
-sampledReplica20 = sampleTMBOC(replicaBOC11, replicaBOC61, 20e6);
-sampledReplica6 = sampleTMBOC(replicaBOC11, replicaBOC61, 6e6);
-sampledReplica4 = sampleTMBOC(replicaBOC11, replicaBOC61, 4e6);
-sampledReplica2 = sampleTMBOC(replicaBOC11, replicaBOC61, 2e6);
+replica50 = sampleTMBOC(pilotBOC11, pilotBOC61, 50e6);
 
-% Sample at 20 Msps and do autocorrelation
-sampledCode20 = sampleTMBOC(pilotBOC11, pilotBOC61, 20e6);
-autoCorr20 = xcorr(sampledCode20, sampledReplica20);
-autoCorr20Length = (length(autoCorr20) +  1) /2;
-autoCorr20 = autoCorr20(autoCorr20Length : autoCorr20Length * 2 - 1);
+% Full BW autocorrelation
+autoCorr50 = xcorr(replica50, replica50);
+autoCorr50Length = (length(autoCorr50) + 1) /2;
+autoCorr50 = autoCorr50(autoCorr50Length : autoCorr50Length * 2 - 1);
 
-% Resample at 6 Msps and do autocorrelation
-sampledCode6 = resample(sampledCode20, 3, 10);
-autoCorr6 = xcorr(sampledCode6, sampledReplica6);
-autoCorr6Length = (length(autoCorr6) + 1) /2;
-autoCorr6 = autoCorr6(autoCorr6Length : autoCorr6Length * 2 - 1);
+% LP Filter at 5 MHz and do autocorrelation
+filteredCode10 = lowpass(replica50, 5e6, 50e6);
+autoCorr10 = xcorr(filteredCode10, replica50);
+autoCorr10Length = (length(autoCorr10) +  1) /2;
+autoCorr10 = autoCorr10(autoCorr10Length : autoCorr10Length * 2 - 1);
 
-% Resample at 4 Msps and do autocorrelation
-sampledCode4 = resample(sampledCode20, 1, 5);
-autoCorr4 = xcorr(sampledCode4, sampledReplica4);
-autoCorr4Length = (length(autoCorr4) + 1) /2;
-autoCorr4 = autoCorr4(autoCorr4Length : autoCorr4Length * 2 - 1);
+% LP Filter at 2.5 MHz and do autocorrelation
+filteredCode5 = lowpass(replica50, 2.5e6, 50e6);
+autoCorr5 = xcorr(filteredCode5, replica50);
+autoCorr5Length = (length(autoCorr5) +  1) /2;
+autoCorr5 = autoCorr5(autoCorr5Length : autoCorr5Length * 2 - 1);
 
-% Resample at 2 Msps and do autocorrelation
-sampledCode2 = resample(sampledCode20, 1, 10);
-autoCorr2 = xcorr(sampledCode2, sampledReplica2);
+% LP Filter at 1 MHz and do autocorrelation
+filteredCode2 = lowpass(replica50, 1e5, 50e6);
+autoCorr2 = xcorr(filteredCode2, replica50);
 autoCorr2Length = (length(autoCorr2) +  1) /2;
-autoCorr2 = autoCorr2(autoCorr2Length : autoCorr2Length * 2 - 1);
+autoCorr2    = autoCorr2(autoCorr2Length : autoCorr2Length * 2 - 1);
 
 figure(1);
 
 subplot(2,2,1);
-plot(linspace(-2, 2, 81), abs(autoCorr20(200001-40:200001+40)));
-title('AutoCorr of PRN 1 at 20 Msps');
+plot(linspace(-2, 2, 201), abs(autoCorr50(500001-100:500001+100)));
+title('AutoCorr of PRN 1 at 50 MHz BW');
 xlabel('Samples Relative to Peak');
 ylabel('Correlation Energy');
-%xlim([200001-40, 200001+40]);
 
 subplot(2,2,2);
-plot(linspace(-2, 2, 25), abs(autoCorr6(60001-12:60001+12)));
-title('AutoCorr of PRN 1 at Filtered 6 Msps');
+plot(linspace(-2, 2, 201), abs(autoCorr10(500001-100:500001+100)));
+title('AutoCorr of PRN 1 at 10 MHz BW');
 xlabel('Samples Relative to Peak');
 ylabel('Correlation Energy');
 
 subplot(2,2,3);
-plot(linspace(-2, 2, 17), abs(autoCorr4(40001-8:40001+8)));
-title('AutoCorr of PRN 1 at Filtered 4 Msps');
+plot(linspace(-2, 2, 201), abs(autoCorr5(500001-100:500001+100)));
+title('AutoCorr of PRN 1 at 5 MHz BW');
 xlabel('Samples Relative to Peak');
 ylabel('Correlation Energy');
 
 subplot(2,2,4);
-plot(linspace(-2, 2, 9), abs(autoCorr2(20001-4:20001+4)));
-title('AutoCorr of PRN 1 at Filtered 2 Msps');
+plot(linspace(-2, 2, 201), abs(autoCorr2(500001-100:500001+100)));
+title('AutoCorr of PRN 1 at 2 MHz BW');
 xlabel('Samples Relative to Peak');
 ylabel('Correlation Energy');
-%xlim([20001-4, 20001+4]);
+
+figure(2);
+
+subplot(2,2,1);
+plot(linspace(-0.3, 0.3, 31)/codeFreq*c, abs(autoCorr50(500001-15:500001+15)));
+title('Zoomed AutoCorr of PRN 1 at 50 MHz BW');
+xlabel('Pseudorange Relative to Peak [m]');
+ylabel('Correlation Energy');
+
+subplot(2,2,2);
+plot(linspace(-0.3, 0.3, 31)/codeFreq*c, abs(autoCorr10(500001-15:500001+15)));
+title('Zoomed AutoCorr of PRN 1 at 10 MHz BW');
+xlabel('Pseudorange Relative to Peak [m]');
+ylabel('Correlation Energy');
+
+subplot(2,2,3);
+plot(linspace(-0.3, 0.3, 31)/codeFreq*c, abs(autoCorr5(500001-15:500001+15)));
+title('Zoomed AutoCorr of PRN 1 at 5 MHz BW');
+xlabel('Pseudorange Relative to Peak [m]');
+ylabel('Correlation Energy');
+
+subplot(2,2,4);
+plot(linspace(-0.3, 0.3, 31)/codeFreq*c, abs(autoCorr2(500001-15:500001+15)));
+title('Zoomed AutoCorr of PRN 1 at 2 MHz BW');
+xlabel('Pseudorange Relative to Peak [m]');
+ylabel('Correlation Energy');
 
 function pilot_promptCode = sampleTMBOC(pilotBOC11, pilotBOC61, samplingFreq)
     codeFreq = 1.023e6;
